8843a46a2d82c16e8dc2a145386809ab43be8445
Maks <maksimbibki@gmail.com>
2025-11-27 15:01:56 +1100

    Refactor useVantaGlobe to use effectRef
    Refactor useVantaGlobe to use a ref for the effect instead of state.

--- src/hooks/useVantaGlobe.js
+++ src/hooks/useVantaGlobe.js
@@ -5,30 +5,29 @@ import * as THREE from "three";
 
 export default function useVantaGlobe(options = {}) {
   const vantaRef = useRef(null);
-  const [vantaEffect, setVantaEffect] = useState(null);
+  const effectRef = useRef(null); 
 
   useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        GLOBE({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          backgroundColor: 0x050711,
-          color: 0x7c3aed,
-          color2: 0x22d3ee,
-          size: 1.1,
-          ...options,
-        })
-      );
+    if (!effectRef.current && vantaRef.current) {
+      effectRef.current = GLOBE({
+        el: vantaRef.current,
+        THREE,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        backgroundColor: 0x050711,
+        color: 0x7c3aed,
+        color2: 0x22d3ee,
+        size: 1.1,
+        ...options,
+      });
     }
 
+
     return () => {
-      if (vantaEffect) vantaEffect.destroy();
+      
     };
-  }, [vantaEffect, options]);
+  }, []); 
 
   return vantaRef;
 }
